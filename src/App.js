e4276f8d20079ffa626e0c4c6021ce7dceeb00ec
Tara <mulvaneytara@gmail.com>
2021-06-22 11:12:02 -0400

    json data mapped and rendered

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PinData from './PinData.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="pins">
+        {PinData.map(pin => {
+          return (
+            <>
+              <h4>{pin.name}</h4>
+              <p>{pin.description}</p>
+            </>
+          )
+        }) }
+      </div>
     </div>
   );
 }
